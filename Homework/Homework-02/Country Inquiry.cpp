afcc0dd2ffafee3f1e6874b711f56adb934ad1f4
Nicolas Merino <nemerino@cpp.edu>
2026-02-05 12:04:57 -0800

    Adding HW2

---
+++ Homework/Homework-02/Country Inquiry.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+    cout << "What country do you live in?" << endl;
+    string country;
+    cin >> country;
+    cout << "Nice! I've always wanted to visit " << country << "!" << endl;
+
+    return 0;
+}
